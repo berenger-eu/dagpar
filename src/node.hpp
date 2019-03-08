8a93e9409f01c9ee55491a2e0adba7efbf41b4fc
Berenger Bramas <Berenger.Bramas@inria.fr>
2019-03-08 12:59:10 +0100

    ensure not to add this as prev/succ

--- src/node.hpp
+++ src/node.hpp
@@ -46,12 +46,16 @@ public:
 
     void addSuccessor(Node* next){
         //Is not true when index are not in order: assert(id < next->id);
+        assert(next != this);
+        assert(next->id != id);
         assert(std::find(successors.begin(), successors.end(), next) == successors.end());
         successors.push_back(next);
     }
 
     void addPredecessor(Node* prev){
         //Is not true when index are not in order: assert(prev->id < id);
+        assert(prev != this);
+        assert(prev->id != id);
         assert(std::find(predecessors.begin(), predecessors.end(), prev) == predecessors.end());
         predecessors.push_back(prev);
     }
